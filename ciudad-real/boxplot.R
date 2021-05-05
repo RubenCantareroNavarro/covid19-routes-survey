bc110bf062c1b829ed8f8ad94fece201a70f0ea8
Ruben.Cantarero <Ruben.Cantarero@uclm.es>
2021-05-05 17:07:09 +0200

    graficas castellano

--- ciudad-real/boxplot.R
+++ ciudad-real/boxplot.R
@@ -3,13 +3,14 @@
 
 library(readxl)
 
-data <- read_excel("/home/ruben/Escritorio/validation_survey_summary.xlsx")
+data <- read_excel("/home/ruben/repos/covid19-routes-survey/ciudad-real/results/validation_survey_summary_esp.xlsx")
 
 # Danger points
 x = as.numeric(data$survey_number_danger_points)
 y = data$case_id
 
-boxplot(x ~ y, xlab = "Case number", ylab = "Danger points")
+#boxplot(x ~ y, xlab = "Case number", ylab = "Danger points")
+boxplot(x ~ y, xlab = "Número de caso", ylab = "Puntos peligrosos")
 
 
 
@@ -17,7 +18,7 @@ boxplot(x ~ y, xlab = "Case number", ylab = "Danger points")
 x = as.numeric(data$survey_route_length)
 y = data$case_id
 
-boxplot(x ~ y, xlab = "Case number", ylab = "Danger points")
+boxplot(x ~ y, xlab = "Número de caso", ylab = "Distancia (m)")
 
 
 
@@ -25,4 +26,21 @@ boxplot(x ~ y, xlab = "Case number", ylab = "Danger points")
 x = as.numeric(data$survey_number_danger_points)
 y = data$case_id
 
-boxplot(x, xlab = "Case number")
+# boxplot(x, xlab = "Todos los casos", ylab = "Puntos peligrosos")
+boxplot(x, xlab = "All cases", ylab = "Danger points")
+
+
+
+# Knowledge of the city
+x = as.numeric(data$survey_number_danger_points)
+y = data$knowledge_city_level
+
+boxplot(x ~ y, xlab = "Knowledge of the city", ylab = "Danger points")
+t.test(x ~ y)
+
+# Age
+x = as.numeric(data$survey_number_danger_points)
+y = data$age
+
+boxplot(x ~ y, xlab = "Age", ylab = "Danger points")
+t.test(x ~ y)
